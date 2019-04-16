89f06d2ee55c8724fb6fb9fef70d1855b89d386b
Igor Resman <igor.resman@gmail.com>
2019-04-16 12:17:37 +0200

    Pos system

---
+++ Template BP2/POS_System/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using POS_System.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel() { Window = this, Password = passBox.Password};
+        }
+
+        private void promena(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowViewModel)this.DataContext).Password = passBox.Password;
+        }
+    }
+}
